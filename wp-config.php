c70bacb7148da5ec10f2f778a53410f1e7a9dbc6
Ivan Kristianto <ivan.kristianto@gmail.com>
2014-10-08 02:03:32 +0700

    add plugins

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,7 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 // Custom Content Directory
 // ========================
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/calibrefx/content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
 // ========================
 // Store Revision Up to 5 Only
