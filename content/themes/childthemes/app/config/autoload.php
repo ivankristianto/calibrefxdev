bd2962434c5c18b9dc9eff83162644db7b38a441
Ivan Kristianto <ivan.kristianto@gmail.com>
2014-10-10 09:24:26 +0700

    update calibrefx development

--- content/themes/childthemes/app/config/autoload.php
+++ content/themes/childthemes/app/config/autoload.php
@@ -13,26 +13,9 @@ $autoload['helpers'] = array_merge($autoload['helpers'], array(''));
 /*
  *  Auto-load Config files
  */
-$autoload['configs'] = array_merge($autoload['configs'], array());
-
-/*
- *  Auto-load Config files
- */
-$autoload['widgets'] = array_merge($autoload['widgets'], array(
-								
-						));
-
-/*
- *  Auto-load Config files
- */
 $autoload['models'] = array_merge($autoload['models'], array(''));
 
 /*
  *  Auto-load Config files
  */
 $autoload['hooks'] = array_merge($autoload['hooks'], array('childfx_script','childfx_settings'));
-
-/*
- *  Auto-load Shortcode files
- */
-$autoload['shortcodes'] = array_merge($autoload['shortcodes'], array('shortcode'));
